de9e5b0486757b74c43d93dc241bf8efbbc017c9
Miseung Kim <mieseung@gmail.com>
2021-10-31 19:50:57 +0900

    style: apply pylint

--- backend/gaejosim/user/views.py
+++ backend/gaejosim/user/views.py
@@ -1,9 +1,9 @@
+"""views for user"""
 import json
 import requests
 
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
-from django.contrib.auth.hashers import check_password
 from django.db.utils import IntegrityError
 
 from .models import Summoner, User
@@ -15,12 +15,13 @@ api_default = {
 
 @ensure_csrf_cookie
 def token(request):
+    """set token"""
     if request.method == 'GET':
         return HttpResponse(status=204)
-    else:
-        return HttpResponseNotAllowed(['GET'])
+    return HttpResponseNotAllowed(['GET'])
 
 def sign_up(request):
+    """sign up"""
     if request.method =='POST':
         data = json.loads(request.body.decode())
         username = data['username']
@@ -28,14 +29,16 @@ def sign_up(request):
         summoner_name = data['summoner_name']
         password = data['password']
 
-        validation_url = F"{api_default['region']}/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_default['key']}"
+        validation_url = F"{api_default['region']}\
+            /lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_default['key']}"
+
         validation_req = requests.get(validation_url)
         is_summoner = (validation_req.status_code == 200)
 
         if is_summoner:
             summoner_info = validation_req.json()
             summoner_id = summoner_info['puuid']
-            summoner, is_created = Summoner.objects.get_or_create(summoner_id = summoner_id)
+            summoner, _ = Summoner.objects.get_or_create(summoner_id = summoner_id)
 
             exist = User.objects.filter(summoner = summoner).exists()
 
@@ -45,24 +48,24 @@ def sign_up(request):
                     "error" : "This summoner is already registered in our service"
                 }, status=400
             )
-            
+
             try:
-                user = User.objects.create_user(
+                User.objects.create_user(
                     username = username,
                     email = email,
                     password = password,
                     summoner = summoner
                 )
 
-            except(IntegrityError) as e:
-                if 'username' in str(e):
+            except(IntegrityError) as invalid_input_error:
+                if 'username' in str(invalid_input_error):
                     return JsonResponse(
                     {
                         "error" : "This username already exists."
                     }, status=400
                 )
 
-                if 'email' in str(e):
+                if 'email' in str(invalid_input_error):
                     return JsonResponse(
                     {
                         "error" : "This email already exists."
@@ -71,14 +74,11 @@ def sign_up(request):
 
             return JsonResponse({"message" : "User is created!"}, status=201)
 
+        return JsonResponse(
+            {
+                "error" : "This summoner name is invalid"
+            }, status=400
+        )
 
-        else:
-            return JsonResponse(
-                {
-                    "error" : "This summoner name is invalid"
-                }, status=400
-            )
-        
-    else:
-        HttpResponseNotAllowed(['POST'])
+    return HttpResponseNotAllowed(['POST'])
         
