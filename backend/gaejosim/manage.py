e31d69b3015c34a3856402230193266707cd5bfb
Miseung Kim <mieseung@gmail.com>
2021-10-31 15:39:10 +0900

    feat: mysql setting

--- backend/gaejosim/manage.py
+++ backend/gaejosim/manage.py
@@ -3,9 +3,12 @@
 import os
 import sys
 
+import dotenv
+
 
 def main():
     """Run administrative tasks."""
+    dotenv.read_dotenv()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gaejosim.settings")
     try:
         from django.core.management import execute_from_command_line
