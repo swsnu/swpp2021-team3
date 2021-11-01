67b1ed568db35cc95182822e8553881aff8ef9bd
Miseung Kim <mieseung@gmail.com>
2021-11-02 01:09:29 +0900

    feat: user model & migration files

--- backend/gaejosim/gaejosim/settings.py
+++ backend/gaejosim/gaejosim/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user.apps.UserConfig"
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'user.User'
