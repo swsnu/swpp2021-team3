22ace8ba8e76133e9be9f78e196b93c6cc9051a7
Junetrbl <junepark1211@snu.ac.kr>
2021-11-02 14:22:54 +0900

    update urls.py to add signup API

--- backend/gaejosim/user/urls.py
+++ backend/gaejosim/user/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("signin/", views.sign_in, name="signin"),
+    path("signup/", views.sign_up, name="signup"),
     path("token/", views.token, name="token"),
 ]
