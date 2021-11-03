e37223f4736a1ab36b4dd5575a7b7f41d4315fb5
Miseung Kim <mieseung@gmail.com>
2021-11-03 18:44:01 +0900

    feat: add report model and migrations files

--- backend/gaejosim/gaejosim/settings.py
+++ backend/gaejosim/gaejosim/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "user",
+    "report"
 ]
 
 AUTH_USER_MODEL = "user.User"
--- backend/gaejosim/report/models.py
+++ backend/gaejosim/report/models.py
@@ -1,3 +1,19 @@
+"""models for report"""
 from django.db import models
+from user.models import Summoner
 
-# Create your models here.
+
+class Report(models.Model):
+    """Summoner model"""
+    tag = models.TextField()
+    comment = models.CharField(max_length=2000)
+    reported_summoner = models.OneToOneField(
+        Summoner, on_delete=models.CASCADE, related_name='reported'
+    )
+    reported_user = models.OneToOneField(
+        Summoner, on_delete=models.CASCADE, related_name='reporting'
+    )
+    evaluation = models.IntegerField()
+    match_id = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
