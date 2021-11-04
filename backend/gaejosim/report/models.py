f0ff2700750c6c637cb1b5b8852bc3bc5352b559
Miseung Kim <mieseung@gmail.com>
2021-11-04 23:41:53 +0900

    fix: change Report model & migration

--- backend/gaejosim/report/models.py
+++ backend/gaejosim/report/models.py
@@ -12,9 +12,8 @@ class Report(models.Model):
         Summoner, on_delete=models.CASCADE, related_name="reported"
     )
     reporting_user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name="reporting"
+        User, on_delete=models.CASCADE, related_name="reporting", null=True
     )
     evaluation = models.IntegerField()
-    # match_id = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
