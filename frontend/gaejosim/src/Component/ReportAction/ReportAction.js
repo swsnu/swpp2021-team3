be167de9a2e972176a2f2ebb4991e32edc6b0ef3
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-07 19:21:33 +0900

    reportAction view

--- frontend/gaejosim/src/Component/ReportAction/ReportAction.js
+++ frontend/gaejosim/src/Component/ReportAction/ReportAction.js
@@ -3,9 +3,31 @@ import React, { Component } from "react";
 const ReportAction= (props) => {
     return (
         <div className='ReportAction'>            
-
             <h3>ReportAction Component</h3>
-
+            <div>
+                <h3>2. Manner Point</h3>
+                <textarea> Manner Point Graph Image</textarea>
+                <div><text>text details about manner point</text></div>
+            </div>
+            <div>
+                <h3>3.Tags</h3>
+                <button>#tag1</button>
+                <button>#tag1</button>
+                <button>#tag1</button>
+                <button>#tag1</button>
+                <button>#tag1</button>
+                <button>#tag1</button>
+                <button>#tag1</button>
+                <button>#tag1</button>
+            </div>
+            <div>
+                <h3>4. Comment(Optional)</h3>
+                <div><text>my name is blah blah blah</text></div>
+                <div><input type="text" placeholder="Type your text contents here" /></div>
+            </div>
+            {/* <button>Submit</button> */}
+            <input type ="submit" value="Submit" />
+            {/* <button onClick = {() => navigate(-2)}>Cancel</button> */}
         </div>
     )
 }
