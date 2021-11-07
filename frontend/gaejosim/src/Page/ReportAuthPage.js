08a96719232eb4f9b2fe13f1b05b6df1afc9e0b6
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-07 16:47:24 +0900

    report auth to report action next button

--- frontend/gaejosim/src/Page/ReportAuthPage.js
+++ frontend/gaejosim/src/Page/ReportAuthPage.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
+import { Redirect } from 'react-router-dom';
 
 import Header from '../Container/Header/Header';
 
 class ReportAuthPage extends Component {
 
+    state = {
+        clickReportAction : false
+    }
+
+    onClickReportActionButton = () => {
+        this.setState({ clickReportAction: true });
+    }
+
     render () {
+        let redirect = null;
+        if (this.state.clickReportAction) {
+            redirect = <Redirect to = '/reportAction' />
+        }
         return (
             <div className='ReportAuthPage'>
+                {redirect}
                 <Header />
                 ReportAuthPage
+                <button onClick={() => this.onClickReportActionButton()}>Next</button>
             </div>
         )
     }
