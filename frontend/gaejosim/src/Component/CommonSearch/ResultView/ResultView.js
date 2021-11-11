269f1a3fbbb4a19087d6b34b8837d81acab03341
Miseung Kim <mieseung@gmail.com>
2021-11-11 13:15:41 +0900

    fix: app logo

--- frontend/gaejosim/src/Component/CommonSearch/ResultView/ResultView.js
+++ frontend/gaejosim/src/Component/CommonSearch/ResultView/ResultView.js
@@ -8,10 +8,10 @@ const ResultView = (props) => {
         <div className='resultView'>
             {/* W이나 L에 따라 색상 다르게 처리 */}
             {(props.win_lose === "W") &&
-                <Result result={props.recent_result} />
+                <Result result={props.recent_result} win_lose={props.win_lose} />
             }
             {(props.win_lose === "L") &&
-                <Result result={props.recent_result} />
+                <Result result={props.recent_result} win_lose={props.win_lose} />
             }
         </div>
     )
