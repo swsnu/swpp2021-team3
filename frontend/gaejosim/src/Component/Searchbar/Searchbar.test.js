06e0c2fc826d2c6a4770930334cd1acacf993bde
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-13 12:37:16 +0900

    #70 feature/buttontest (#71)
    * fix: button test code
    
    * fix: className

--- frontend/gaejosim/src/Component/Searchbar/Searchbar.test.js
+++ frontend/gaejosim/src/Component/Searchbar/Searchbar.test.js
@@ -8,14 +8,20 @@ describe('<Searchbar />', () => {
       const wrapper = component.find('.Searchbar');
       expect(wrapper.length).toBe(1);
     });
+
+    it('should handle search button', () => {
+      const mockSearchButton = jest.fn();
+      const component = shallow(<Searchbar clickDone={mockSearchButton} />);
+      const wrapper = component.find('.search');
+      wrapper.simulate('click');
+      expect(mockSearchButton).toHaveBeenCalledTimes(0);
+    });
+
+    it('should handle single or multi button', () => {
+      const mockSingleOrMultiButton = jest.fn();
+      const component = shallow(<Searchbar clickDone={mockSingleOrMultiButton} />);
+      const wrapper = component.find('.singleormulti');
+      wrapper.simulate('click');
+      expect(mockSingleOrMultiButton).toHaveBeenCalledTimes(0);
+    });
 });
-
-// clickSearchButton
-
-// clickSingleOrMultiButton
-
-// parseSummoner
-
-// Singlesearch router
-
-// Multi search router
