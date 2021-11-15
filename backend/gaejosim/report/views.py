2d69a8f75e40bedca2cc04d7012202832157be92
Junetrbl <junepark1211@snu.ac.kr>
2021-11-15 09:14:29 +0900

    fix: update RIOT API key (#82)

--- backend/gaejosim/report/views.py
+++ backend/gaejosim/report/views.py
@@ -11,7 +11,7 @@ api_default = {
     "region": "https://kr.api.riotgames.com",
     "asia": "https://asia.api.riotgames.com",  # korea server
     # api key : needs to regenerate every 24hr
-    "key": "RGAPI-65a865e9-4b0f-4fcb-a51d-f26b05960485",  # updated 11/13 14:40
+    "key": "RGAPI-c7b1091b-f232-4184-ba59-1eaaf0e5466e",  # updated 11/15 08:40
 }
 
 tag_dict = {
@@ -126,8 +126,7 @@ def post_report(request):
     reported_summoner_puuid = reported_summoner_json["puuid"]
 
     if Summoner.objects.filter(summoner_puuid=reported_summoner_puuid).exists():
-        reported_summoner = Summoner.objects.get(
-            summoner_puuid=reported_summoner_puuid)
+        reported_summoner = Summoner.objects.get(summoner_puuid=reported_summoner_puuid)
     else:
         reported_manner_point = MannerPoint.objects.create()
         reported_summoner = Summoner.objects.create(
@@ -146,8 +145,7 @@ def post_report(request):
 
     # apply to manner point
     manner_point = reported_summoner.manner_point
-    reports_cnt = Report.objects.filter(
-        reported_summoner=reported_summoner).count()
+    reports_cnt = Report.objects.filter(reported_summoner=reported_summoner).count()
     manner_point.point = (manner_point.point * reports_cnt + evaluation) / (
         reports_cnt + 1
     )
--- backend/gaejosim/search/views.py
+++ backend/gaejosim/search/views.py
@@ -8,7 +8,7 @@ api_default = {
     "asia": "https://asia.api.riotgames.com",  # asia server
     "korea": "https://kr.api.riotgames.com",  # korea server
     # api key : needs to regenerate every 24hr
-    "key": "RGAPI-65a865e9-4b0f-4fcb-a51d-f26b05960485",  # updated 11/13 14:40
+    "key": "RGAPI-c7b1091b-f232-4184-ba59-1eaaf0e5466e",  # updated 11/15 08:40
 }
 
 
@@ -51,8 +51,7 @@ def search(request):
         if summoner_league_req.json() != []:
             for league_dto in summoner_league_req.json():
                 if league_dto["queueType"] == "RANKED_SOLO_5x5":
-                    tier = {"tier": league_dto["tier"],
-                            "rank": league_dto["rank"]}
+                    tier = {"tier": league_dto["tier"], "rank": league_dto["rank"]}
                     break
 
         matches_by_summoner_url = (
