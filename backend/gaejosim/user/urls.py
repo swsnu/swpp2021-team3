04f2c72ee623962024d0b5700051c0aaabbb801c
2018-10036 <junepark1211@snu.ac.kr>
2021-11-22 09:13:49 +0900

    feature: Logout

--- backend/gaejosim/user/urls.py
+++ backend/gaejosim/user/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     path("signin/", views.sign_in, name="signin"),
     path("signup/", views.sign_up, name="signup"),
+    path("logout/", views.log_out, name="logout"),
     path("token/", views.token, name="token"),
     path("change/password/", views.change_password, name="change_password"),
     path("forgot/id/", views.find_username, name="find username"),
-    path("forgot/password/", views.find_password, name="find password")
+    path("forgot/password/", views.find_password, name="find password"),
 ]
