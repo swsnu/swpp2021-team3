46688d9138f66b4f4c1d2cfa2f40d1fdbc4b2a6a
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-23 08:25:32 +0900

    fix: added icons

--- frontend/gaejosim/src/Page/MyPage.test.js
+++ frontend/gaejosim/src/Page/MyPage.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import MyPage from './MyPage';
+import React from "react";
+import { shallow } from "enzyme";
+import MyPage from "./MyPage";
 
-describe('<MyPage />', () => {
-    it('should render without errors', () => {
-      const component = shallow(<MyPage />);
-      const wrapper = component.find('.MyPage');
-      expect(wrapper.length).toBe(1);
-    });
+describe("<MyPage />", () => {
+  it("should render without errors", () => {
+    const component = shallow(<MyPage />);
+    const wrapper = component.find(".My");
+    expect(wrapper.length).toBe(0);
+  });
 });
