dde6c35460b7e5330347234677dd6743c16d5efb
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-24 22:38:13 +0900

    fix: search page buttons css styling

--- frontend/gaejosim/src/Page/FindUserInfoPage.js
+++ frontend/gaejosim/src/Page/FindUserInfoPage.js
@@ -1,17 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import Header from '../Container/Header/Header';
+import Header from "../Container/Header/Header";
 
 class FindUserInfoPage extends Component {
-
-    render () {
-        return (
-            <div className='FindUserInfoPage'>
-                <Header />
-                FindUserInfoPage
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="FindUserInfoPage">
+        <Header />
+        FindUserInfoPage
+      </div>
+    );
+  }
 }
 
 export default FindUserInfoPage;
