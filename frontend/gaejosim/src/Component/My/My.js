d87c8908a031f9466275eb8d84a0f79b28f9b71f
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-24 22:15:10 +0900

    fix: axios of mypage

--- frontend/gaejosim/src/Component/My/My.js
+++ frontend/gaejosim/src/Component/My/My.js
@@ -1,18 +1,74 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-// import axios from "axios";
+import axios from "axios";
 import "./My.css";
 import Pencil from "../../Assets/Images/icon-pencil.png";
 import Paper from "../../Assets/Images/icon-paper.png";
 import Delete from "../../Assets/Images/icon-delete.png";
 
+//TODO: mypage + apology backend 구현되면 + 서버 돌아가는 것 확인하면 확실하게 개발하기
+
 class My extends Component {
-  state = {
-    ApologyCheck: false,
-    ApologyWrite: false,
-    ReportedLogs: false,
-    ReportingLogs: false,
-    // DeleteReport: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      // user model
+      username: props.username,
+      email: props.email,
+      summoner_name: props.summoner_name,
+      manner_point: props.manner_point,
+
+      reports: [],
+
+      getMyPageDataBoolean: false,
+
+      // button routers
+      ApologyCheck: false,
+      ApologyWrite: false,
+      ReportedLogs: false,
+      ReportingLogs: false,
+      // DeleteReport: false,
+    };
+  }
+
+  // state = {
+  //   ApologyCheck: false,
+  //   ApologyWrite: false,
+  //   ReportedLogs: false,
+  //   ReportingLogs: false,
+  //   // DeleteReport: false,
+  // };
+
+  getMyPageData = async () => {
+    console.log("call of getMyPageData");
+    console.log("state of getMyPageData: " + this.state.getMyPageDataBoolean);
+
+    axios.defaults.xsrfCookieName = "csrftoken";
+    axios.defaults.xsrfHeaderName = "X-CSRFToken";
+
+    console.log(this.state.username);
+    console.log(this.state.email);
+    console.log(this.state.summoner_name);
+    console.log(this.state.manner_point);
+
+    axios.get("/api/token/").then();
+
+    const url = `http://localhost:3000/api/me/mypage`;
+    console.log("call get request");
+
+    const response = await axios.get(url, {
+      params: {
+        username: this.state.username,
+        email: this.state.email,
+        summoner_name: this.state.summoner_name,
+        manner_point: this.state.manner_point,
+      },
+    });
+
+    this.setState({
+      reports: response.data.reports,
+      getMyPageDataBoolean: true,
+    });
   };
 
   onClickApologyCheck = () => {
@@ -60,20 +116,21 @@ class My extends Component {
         <text className="mypageTitle">마이페이지</text>
         <text className="mypageContent">
           <text style={{ fontWeight: "bold" }}>유저네임</text>
-          <br /> XXXXXXXX
+          <br /> XXXXXXXX {this.state.username}
           <br />
           <br />
           <text style={{ fontWeight: "bold" }}>이메일</text>
           <br />
-          Email@email.com
+          Email@email.com {this.state.email}
           <br />
           <br />
           <text style={{ fontWeight: "bold" }}>소환사이름</text>
           <br />
-          XXXXXXXX
+          XXXXXXXX {this.state.summoner_name}
           <br />
           <br />
-          <text style={{ fontWeight: "bold" }}>매너포인트</text> 80%
+          <text style={{ fontWeight: "bold" }}>매너포인트</text> 80%{" "}
+          {this.state.manner_point}
           <div className="mpGraphPercent" />
           <div className="mpGraphAll" />
         </text>
