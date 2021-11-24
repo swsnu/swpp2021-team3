93627c7c4d270528509703b651c412503913458e
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-24 22:44:59 +0900

    fix: front test error code solved

--- frontend/gaejosim/src/Component/Statistic/Statistic.test.js
+++ frontend/gaejosim/src/Component/Statistic/Statistic.test.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Statistic from './Statistic';
-import {render, screen} from '@testing-library/react';
+import React from "react";
+import { shallow } from "enzyme";
+import Statistic from "./Statistic";
+import { render, screen } from "@testing-library/react";
 
-describe('<Statistic />', () => {
-    it('should render without errors', () => {
-      const component = shallow(<Statistic />);
-      const wrapper = component.find('.Statistic');
-      expect(wrapper.length).toBe(1);
-    });
+describe("<Statistic />", () => {
+  it("should render without errors", () => {
+    const component = shallow(<Statistic />);
+    const wrapper = component.find(".Statistic");
+    expect(wrapper.length).toBe(1);
+  });
 
   //   it('should render states without errors.', () => {
   //     const wrapper = shallow(<Statistic />);
@@ -16,8 +16,8 @@ describe('<Statistic />', () => {
   // });
 });
 
-test('renders Statistic page', () => {
+test("renders Statistic page", () => {
   render(<Statistic />);
-  const linkElement = screen.getByText(/Reports/);
+  const linkElement = screen.getByText(/Today/);
   expect(linkElement).toBeInTheDocument();
 });
