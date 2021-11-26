4505731b7e8657f4847cb5469931c51a59e4dbcd
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-26 09:26:45 +0900

    fix: setting

--- frontend/gaejosim/src/Page/MultiSearchResultPage.js
+++ frontend/gaejosim/src/Page/MultiSearchResultPage.js
@@ -1,34 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import Header from '../Container/Header/Header';
-import Searchbar from '../Component/Searchbar/Searchbar';
-import MultiSearch from '../Component/MultiSearch';
+import Header from "../Container/Header/Header";
+import Searchbar from "../Component/Searchbar/Searchbar";
+import MultiSearch from "../Component/MultiSearch";
 
-
-import './MultiSearchResultPage.css';
+import "./MultiSearchResultPage.css";
 
 class MultiSearchResultPage extends Component {
+  state = {
+    summoners_str: "",
+  };
 
-    state = {
-        summoners_str : ''
-    }
+  render() {
+    const path = this.props.location.pathname;
+    const splitResult = path.split("/");
+    const summonerList = splitResult[2].split("-");
+    this.state.summoners_str = summonerList.join(",");
 
-    render () {
-        const path = this.props.location.pathname; 
-        const splitResult = path.split('/');
-        const summonerList = splitResult[2].split('-');
-        this.state.summoners_str = summonerList.join(',');
-
-        return (
-            <div className='MultiSearchResultPageMulti'>
-                <Header />
-                <Searchbar />
-                {/* <h1>MultiSearchResultPage</h1> */}
-                <text className='titleTextStyleMulti'>MultiSearchResultPage</text>
-                <MultiSearch summoners={this.state.summoners_str}/>
-            </div>
-        )
-    }
+    return (
+      <div className="MultiSearchResultPageMulti">
+        <Header />
+        <Searchbar />
+        {/* <h1>MultiSearchResultPage</h1> */}
+        <text className="titleTextStyleMulti">MultiSearchResultPage</text>
+        <MultiSearch summoners={this.state.summoners_str} />
+      </div>
+    );
+  }
 }
 
 export default MultiSearchResultPage;
