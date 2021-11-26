277aeeffd396da08ba59dd800023447ee3c28d73
ericagong <48196721+ericagong@users.noreply.github.com>
2021-11-26 12:26:13 +0900

    change of report auth page

--- frontend/gaejosim/src/Page/ReportAuthPage.js
+++ frontend/gaejosim/src/Page/ReportAuthPage.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-// import { Redirect } from 'react-router-dom';
 
 import Header from '../Container/Header/Header';
 import ReportAuth from '../Component/ReportAuth/ReportAuth';
 
 class ReportAuthPage extends Component {
-
-
     render() {
-
         return (
             <div className='ReportAuthPage'>
-
                 <Header />
                 <ReportAuth />
-
             </div>
         )
     }
