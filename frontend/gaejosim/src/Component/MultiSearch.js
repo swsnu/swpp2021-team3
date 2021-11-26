2bbc9302291f3e114ce8176a269dca70c6d5086a
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-26 12:07:47 +0900

    fix: mypage, myreportedlogs page, myreportinglogs page axios code

--- frontend/gaejosim/src/Component/MultiSearch.js
+++ frontend/gaejosim/src/Component/MultiSearch.js
@@ -1,83 +1,80 @@
 import React, { Component } from "react";
 import CommonSearch from "./CommonSearch/CommonSearch";
-import axios from 'axios';
+import axios from "axios";
 
-import './MultiSearch.css';
-
+import "./MultiSearch.css";
 
 class MultiSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      summoners: props.summoners,
+      matchers: [],
+      getResult: false,
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            summoners : props.summoners,
-            matchers : [],
-            getResult : false
-        }
-    }
+  // Get five matchers information by /api/search/ call.
+  getMatchers = async () => {
+    console.log("call of getMatchers");
+    console.log("state of getResult", this.state.getResult);
 
-    // Get five matchers information by /api/search/ call.
-    getMatchers = async () => {
-        console.log("call of getMatchers")
-        console.log("state of getResult", this.state.getResult)
+    axios.defaults.xsrfCookieName = "csrftoken";
+    axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
-        axios.defaults.xsrfCookieName = 'csrftoken';
-        axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+    axios.get("/api/token/").then();
 
-        axios.get('/api/token/').then(
-        )
-        
-        const url = `http://localhost:3000/api/search/`
-        console.log("call get request")
-        const response = await axios.get(url, {
-                    params : {
-                        type : 'multi',
-                        summoners : this.state.summoners
-                    }
-        }) 
+    const url = `http://localhost:3000/api/search/`;
+    console.log("call get request");
+    const response = await axios.get(url, {
+      params: {
+        type: "multi",
+        summoners: this.state.summoners,
+      },
+    });
 
-        this.setState({
-            matchers : response.data.matchers,
-            getResult : true
-        })
-        
-    }
+    this.setState({
+      matchers: response.data.matchers,
+      getResult: true,
+    });
+  };
 
-    render() {
-        let matcherInfos
-        if(this.state.getResult === false) {
-            console.log("call getMatchers")
-            this.getMatchers()
-        }
-        else {
-            let idx = 0
-            console.log("finished")
-            matcherInfos = this.state.matchers.map((matcher) => {
-                idx = idx + 1
-                let classIdx = 'class' + idx
-                return (
-                <div className = {classIdx}>
-                    <CommonSearch 
-                        summoner_name = {matcher.summoner_name} 
-                        tier = {matcher.tier}
-                        rank = {matcher.rank}
-                        manner_point = {matcher.manner_point}
-                        tag_values = {matcher.tag_values}
-                        win_lose = {matcher.win_lose}
-                        recent_result = {matcher.recent_result}
-                        num = {idx}
-                    />
-                </div>
-                )
-            }) 
-        }
+  render() {
+    let matcherInfos;
+    if (this.state.getResult === false) {
+      console.log("call getMatchers");
+      this.getMatchers();
+    } else {
+      let idx = 0;
+      console.log("finished");
+      matcherInfos = this.state.matchers.map((matcher) => {
+        idx = idx + 1;
+        let classIdx = "class" + idx;
         return (
-            <div className='MultiSearch'>
-                {(!this.state.getResult) && <div className = 'loading'>loading...</div>}
-                {(this.state.getResult) && <div className='matchInfos'>{matcherInfos}</div>}
-            </div>
-        )
+          <div className={classIdx}>
+            <CommonSearch
+              summoner_name={matcher.summoner_name}
+              tier={matcher.tier}
+              rank={matcher.rank}
+              manner_point={matcher.manner_point}
+              tag_values={matcher.tag_values}
+              win_lose={matcher.win_lose}
+              recent_result={matcher.recent_result}
+              num={idx}
+            />
+          </div>
+        );
+      });
     }
+    return (
+      <div className="MultiSearch">
+        {!this.state.getResult && <div className="loading">loading...</div>}
+        {this.state.getResult && (
+          <div className="matchInfos">{matcherInfos}</div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default MultiSearch;
