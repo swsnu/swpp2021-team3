0012ee4e4656fba68fe0ec95a23405a8b08682a2
ericagong <48196721+ericagong@users.noreply.github.com>
2021-11-27 07:55:12 +0900

    userReducer

---
+++ frontend/gaejosim/src/Store/Reducers/UserReducer.js
@@ -0,0 +1,18 @@
+import * as actionTypes from '../Actions/ActionTypes';
+
+const initialUserState = {
+    login : false
+}
+
+const userReducer = (state = initialUserState, action) => {
+    switch(action.type) {
+        case actionTypes.SIGNIN_USER:
+            return { ...state, login : true }
+        case actionTypes.SIGNOUT_USER:
+            return { ...state, login : false }
+        default:
+            return state
+    }
+}
+
+export default userReducer
