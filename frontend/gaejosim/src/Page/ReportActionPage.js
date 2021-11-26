89c0c54845168e232abe9ddf0c0347eb1ad108e0
ericagong <48196721+ericagong@users.noreply.github.com>
2021-11-26 14:33:58 +0900

    #126 feature/report (#128)
    * check git status
    
    * initial state
    
    * delete singleSearch Page
    
    * make app.js code clean
    
    * change multiSearchResultPage code
    
    * delete reportlist.js
    
    * change multiSearch component naming
    
    * code smell : repetitive code on commonsearch component
    
    * delete resultview componenet
    
    * handling code smell : repetitive code on result component
    
    * handling parsing cornercase
    
    * handle multisearch result visibility
    
    * result color corresponding to winLose result
    
    * update secret key
    
    * change router of reportActionPage
    
    * change of report auth page
    
    * implementation of select in reportauth component
    
    * mui on reportauth
    
    * handling code smell: repetitive code
    
    * change mannerpoint field to MUI
    
    * add todos
    
    * Add progressbar component to reportauth and reportaction page

--- frontend/gaejosim/src/Page/ReportActionPage.js
+++ frontend/gaejosim/src/Page/ReportActionPage.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 
 import Header from '../Container/Header/Header';
+import Progressbar from '../Component/Progressbar/Progressbar';
 import ReportAction from '../Component/ReportAction/ReportAction';
+
 import './ReportActionPage.css';
 
 class ReportActionPage extends Component {
 
     render() {
-        const path = this.props.location.pathname;
-        const splitResult = path.split('/');
-        const reported_summoner = splitResult[2];
-        console.log("reported summoner", reported_summoner)
+        const reportedSummoner = this.props.match.params.summonerID
+        console.log("name", reportedSummoner)
 
         return (
             <div className='ReportActionPage'>
                 <Header />
-                <p id="ReportPageName">Report</p>
-                <ReportAction reported_summoner={reported_summoner} />
+                <Progressbar process="2"/>
+                <ReportAction reported_summoner={reportedSummoner} />
             </div>
         )
     }
 }
 
-export default ReportActionPage;
+export default ReportActionPage
