3aebb7934ff79e4f784910a6d7ff7a0a76f9eaa6
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-11-27 11:42:33 +0900

    fix: login page css

--- frontend/gaejosim/src/Container/Login/Login.js
+++ frontend/gaejosim/src/Container/Login/Login.js
@@ -1,75 +1,87 @@
 import React, { Component } from "react";
 // import { connect } from 'react-redux';
-import { NavLink, withRouter } from 'react-router-dom'
-import axios from 'axios';
+import { NavLink, withRouter } from "react-router-dom";
+import axios from "axios";
+
+import "./Login.css";
 
 // TODO: login 유저 스토어에 저장하기
 
 class Login extends Component {
-    state = {
-        id : '',  
-        password : '', 
+  state = {
+    id: "",
+    password: "",
+  };
+
+  postLoginData = async () => {
+    console.log("postSignUpData");
+
+    axios.defaults.xsrfCookieName = "csrftoken";
+    axios.defaults.xsrfHeaderName = "X-CSRFToken";
+
+    axios.get("/api/token/").then();
+
+    const response = await axios
+      .post("/api/signin/", {
+        username: this.state.id,
+        password: this.state.password,
+      })
+      .then((response) => {
+        console.log("로그인 완료");
+        this.props.history.push("/search");
+      })
+      .catch((error) => {
+        alert(error.response.data.error);
+      });
+  };
+
+  onClickLoginButton = () => {
+    if (!this.state.id) {
+      alert("아이디를 입력해야합니다.");
+      return;
+    } else if (!this.state.password) {
+      alert("패스워드를 입력해야합니다.");
+      return;
+    } else {
+      this.postLoginData();
     }
+  };
 
-    postLoginData = async () => {
-        console.log("postSignUpData")
+  render() {
+    return (
+      <div className="Login">
+        <text className="LoginTitle">로그인</text>
+        <NavLink exact to="/signup">
+          <text className="Signup_text">회원가입</text>
+        </NavLink>
+        <NavLink exact to="/finduserinfo">
+          <text className="Finduserinfo_text">아이디 비밀번호 찾기</text>
+        </NavLink>
+        <NavLink exact to="/changepassword">
+          <text className="Changepassword_text">비밀번호 변경</text>
+        </NavLink>
 
-        axios.defaults.xsrfCookieName = 'csrftoken';
-        axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-
-        axios.get('/api/token/').then()
-
-        const response = await axios.post('/api/signin/', {
-            "username" : this.state.id,
-	        "password" : this.state.password,
-        })
-        .then((response) => {
-            console.log("로그인 완료")
-            this.props.history.push('/search')
-        })
-        .catch((error) => {
-            alert(error.response.data.error)
-        })
-    }
-
-    onClickLoginButton = () => {
-        if(!this.state.id) {
-            alert('아이디를 입력해야합니다.')
-            return
-        }
-        else if(!this.state.password) {
-            alert('패스워드를 입력해야합니다.')
-            return
-        }
-        else {
-            this.postLoginData()
-        }
-    }
-
-    render() {
-        return (
-            <div className = 'Login'>
-                <NavLink exact to = '/signup'>회원가입</NavLink>
-                <NavLink exact to = '/finduserinfo'>아이디 비밀번호 찾기</NavLink>
-                <NavLink exact to = '/changepassword'>비밀번호 변경</NavLink>
-                
-                <input
-                    className = 'inputField'
-                    type = 'string'
-                    placeholder = '아이디'
-                    onChange={(event) => this.setState({ id : event.target.value })} />
-                <input
-                    className = 'inputField'
-                    type = 'string'
-                    placeholder = '비밀번호'
-                    onChange={(event) => this.setState({ password : event.target.value })} />
-                <button className = 'LoginButton'
-                    onClick={() => this.onClickLoginButton()}>
-                    로그인
-                </button>
-            </div>
-        )
-    }
+        <input
+          className="Login_inputField1"
+          type="string"
+          placeholder="아이디"
+          onChange={(event) => this.setState({ id: event.target.value })}
+        />
+        <input
+          className="Login_inputField2"
+          type="string"
+          placeholder="비밀번호"
+          onChange={(event) => this.setState({ password: event.target.value })}
+        />
+        <button
+          className="LoginButton"
+          onClick={() => this.onClickLoginButton()}
+        >
+          로그인
+        </button>
+      </div>
+    );
+  }
 }
 
-export default withRouter(Login)
+export default withRouter(Login);
