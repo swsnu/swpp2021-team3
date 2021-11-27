2ba7714d53627062a039c796d87a701bf09132f8
Miseung Kim <mieseung@gmail.com>
2021-11-27 15:44:21 +0900

    fix: update docker-back.sh

--- deploy-back.sh
+++ deploy-back.sh
@@ -3,5 +3,6 @@ docker stop backend_container
 docker rm backend_container
 docker rmi backend
 git pull
+cd backend
 docker build -t backend .
 docker run -it -p 0.0.0.0:8000:8000 --name backend_container backend:latest 
