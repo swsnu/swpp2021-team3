fcb4fa242fa37cb109a34e40aea634ea8484930a
Miseung Kim <mieseung@gmail.com>
2021-11-27 15:06:45 +0900

    feat: add deploy shell file

---
+++ deploy-back.sh
@@ -0,0 +1,8 @@
+tar xvf secrets.tar
+cd backend
+source venv/bin/activate
+pip install -r requirements.txt
+cd gaejosim
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
