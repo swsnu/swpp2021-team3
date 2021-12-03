f7269aaca9bb402ca5f731434f3f704abfe61a7c
ericagong <48196721+ericagong@users.noreply.github.com>
2021-12-03 15:31:40 +0900

    axios on mypage

--- frontend/gaejosim/src/Component/My/My.js
+++ frontend/gaejosim/src/Component/My/My.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-// import axios from "axios";
+import {withRouter } from "react-router-dom";
+import axios from "axios";
 
 import ReportedLog from "../ReportedLog/ReportedLog";
 import ReportingLog from "../ReportingLog/ReportingLog";
@@ -8,173 +8,119 @@ import ReportingLog from "../ReportingLog/ReportingLog";
 import "./My.css";
 
 class My extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      // username: props.username,
-      // email: props.email,
-      // summoner_name: props.summoner_name,
-      // manner_point: props.manner_point,
-
-      // reports_for_user: [],
-      // reports_by_user: [],
-
-      // getMyPageDataBoolean: false,
-
-      ReportedLogs: false,
-      ReportingLogs: false,
+      username : '',
+      email : '',
+      summonerName : '',
+      mannerPoint : 0,
+      reportsForUser : [],
+      reportsByUser : [],
+      getResult: false,
     };
   }
 
-  // componentDidMount() {
-  //   if (this.state.getMyPageDataBoolean === false) {
-  //     this.getMyPageData();
-  //   }
-  // }
+  getMyInfo = async () => {
+    axios.defaults.xsrfCookieName = "csrftoken";
+    axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
-  // getMyPageData = async () => {
-  //   axios.defaults.xsrfCookieName = "csrftoken";
-  //   axios.defaults.xsrfHeaderName = "X-CSRFToken";
+    axios.get("/api/token/").then();
 
-  //   axios.get("/api/token/").then();
-
-  //   const response_signin = await axios.post("/api/signin/", {
-  //     username: "test1",
-  //     password: "password",
-  //   });
-
-  //   if (response_signin.status === 200) {
-  //     const response = await axios.get("/api/me/mypage/");
-
-  //     this.setState({
-  //       username: response.data.username,
-  //       email: response.data.email,
-  //       summoner_name: response.data.summoner_name,
-  //       manner_point: response.data.manner_point,
-  //       reports_for_user: response.data.reports_for_user,
-  //       reports_by_user: response.data.reports_by_user,
-  //       getMyPageDataBoolean: true,
-  //     });
-  //   }
-  // };
+    console.log("getMyInfo")
+    const response = await axios.get('/api/mypage/', {  
+    })
+    .then(res => {
+      let userInfo = res.data.user
+      let reportInfo = res.data.reports
+      this.setState({username: userInfo.username, email: userInfo.email, summonerName: userInfo.summoner_name, mannerPoint: userInfo.mannerPoint,
+          reportsForUser: reportInfo.reports_for_user, reportsByUser : reportInfo.reports_by_user, getResult : true
+      })
+    })
+  };
 
   onClickReportedLogs = () => {
-    this.setState({ ReportedLogs: true });
+    this.props.history.push('/myReportedLogs')
   };
 
   onClickReportingLogs = () => {
-    this.setState({ ReportingLogs: true });
+    this.props.history.push('/myReportingLogs')
   };
 
   render() {
-    let redirect = null;
-
-    // let reports_by_user_info;
-    // let reports_for_user_info;
-    // if (
-    //   this.state.reports_by_user_info === false ||
-    //   this.state.reports_for_user_info === false
-    // ) {
-    //   this.getMyPageData();
-    // } else {
-    //   reports_by_user_info = this.state.reports_by_user.map(
-    //     (reports_by_user) => {
-    //       return (
-    //         <div className={reports_by_user.id}>
-    //           <div>
-    //             Recent Reporting Log{reports_by_user.id}:{" "}
-    //             {reports_by_user.reported_summoner}
-    //           </div>
-    //           <div>
-    //             {reports_by_user.tag}, {reports_by_user.evaluation},{" "}
-    //             {reports_by_user.comment}
-    //           </div>
-    //         </div>
-    //       );
-    //     }
-    //   );
-    //   reports_for_user_info = this.state.reports_for_user.map(
-    //     (reports_for_user) => {
-    //       return (
-    //         <div className={reports_for_user.id}>
-    //           <div>
-    //             Recent Reporting Log{reports_for_user.id}:
-    //             {reports_for_user.reported_summoner}
-    //           </div>
-    //           <div>
-    //             {reports_for_user.tag}, {reports_for_user.evaluation},
-    //             {reports_for_user.comment}
-    //           </div>
-    //         </div>
-    //       );
-    //     }
-    //   );
-    // }
-
-    if (this.state.ReportedLogs === true) {
-      redirect = <Redirect to={`/myReportedLogs`} />;
+    let reportedLogs = []
+    let reportingLogs = []
+    if (this.state.getResult === false) {
+      this.getMyInfo()
+    } 
+    else {
+      let idx = 0
+      reportedLogs = this.state.reportsForUser.map((report) => {
+        idx++
+        return (
+          <ReportedLog key={idx} userID={report.id} userEvaluation={report.userEvaluation}
+            tags={report.tags} comment={report.comment} apology={report.apology}
+          />
+        )
+      })
     }
-
-    if (this.state.ReportingLogs === true) {
-      redirect = <Redirect to={`/myReportingLogs`} />;
-    }
-
     return (
       <div className="myPage">
-        {redirect}
-        <text className="mypageTitle">마이페이지</text>
-        <text className="mypageContent">
-          <text style={{ fontWeight: "bold" }}>유저네임</text>
-          <br /> this.state.username
-          <br />
-          <br />
-          <text style={{ fontWeight: "bold" }}>이메일</text>
-          <br />
-          this.state.email
-          <br />
-          <br />
-          <text style={{ fontWeight: "bold" }}>소환사이름</text>
-          <br />
-          this.state.summoner_name
-          <br />
-          <br />
-          <text style={{ fontWeight: "bold" }}>매너포인트</text>
-          <br />
-          this.state.manner_point
-        </text>
-        <div style={{ left: "38.5%" }}>
-          <text className="recentText1">Recent Reporting Logs</text>
-          <text
-            className="SeemoreText1"
-            onClick={() => this.onClickReportingLogs()}
-          >
-            더보기
-          </text>
-          <div className="mypage_box1">
-            <ReportingLog />
+        <div className="mypageTitle">마이페이지</div>
+        {this.state.getResult && 
+          <div className = 'resultDisplay'>
+            <div className="mypageContent">
+              <div style={{ fontWeight: "bold" }}>유저네임</div>
+              {this.state.username}
+              <br />
+              <br />
+              <div style={{ fontWeight: "bold" }}>이메일</div>
+              {this.state.email}
+              <br />
+              <br />
+              <div style={{ fontWeight: "bold" }}>소환사이름</div>
+              {this.state.summonerName}
+              <br />
+              <br />
+              <div style={{ fontWeight: "bold" }}>매너포인트</div>
+              {this.state.mannerPoint}
+            </div>
+            <div style={{ left: "38.5%" }}>
+              <div className="recentText1">Recent Reporting Logs</div>
+              <div
+                className="SeemoreText1"
+                onClick={() => this.onClickReportingLogs()}
+              >
+                더보기
+              </div>
+              <div className="mypage_box1">
+                {reportedLogs}
+              </div>
+              <div className="mypage_box2">
+                <ReportingLog />
+              </div>
+            </div>
+            <div>
+              <div className="recentText2">Recent Reported Logs</div>
+              <div
+                className="SeemoreText2"
+                onClick={() => this.onClickReportedLogs()}
+              >
+                더보기
+              </div>
+              <div className="mypage_box3">
+                <ReportedLog />
+              </div>
+              <div className="mypage_box4">
+                <ReportedLog />
+              </div>
+            </div>
           </div>
-          <div className="mypage_box2">
-            <ReportingLog />
-          </div>
-        </div>
-        <div>
-          <text className="recentText2">Recent Reported Logs</text>
-          <text
-            className="SeemoreText2"
-            onClick={() => this.onClickReportedLogs()}
-          >
-            더보기
-          </text>
-          <div className="mypage_box3">
-            <ReportedLog />
-          </div>
-          <div className="mypage_box4">
-            <ReportedLog />
-          </div>
-        </div>
+        }
       </div>
     );
   }
 }
 
-export default My;
+export default withRouter(My)
--- frontend/gaejosim/src/Component/ReportedLog/ReportedLog.js
+++ frontend/gaejosim/src/Component/ReportedLog/ReportedLog.js
@@ -1,113 +1,27 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-// import axios from "axios";
-import Pencil from "../../Assets/Images/icon-pencil.png";
 
 import "./ReportedLog.css";
 
 class ReportedLog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // username: props.username,
-      // email: props.email,
-      // summoner_name: props.summoner_name,
-      // manner_point: props.manner_point,
-
-      // reports_for_user: [],
-      // reports_by_user: [],
-
-      // getMyPageDataBoolean: false,
-
-      // ApologyCheck: false,
-      ApologyWrite: false,
-      // ReportedLogs: false,
-      // ReportingLogs: false,
-    };
-  }
-
-  // componentDidMount() {
-  //   if (this.state.getMyPageDataBoolean === false) {
-  //     this.getMyPageData();
-  //   }
-  // }
-
-  // getMyPageData = async () => {
-  //   axios.defaults.xsrfCookieName = "csrftoken";
-  //   axios.defaults.xsrfHeaderName = "X-CSRFToken";
-
-  //   axios.get("/api/token/").then();
-
-  //   const response_signin = await axios.post("/api/signin/", {
-  //     username: "test1",
-  //     password: "password",
-  //   });
-
-  //   if (response_signin.status === 200) {
-  //     const response = await axios.get("/api/me/mypage/");
-
-  //     this.setState({
-  //       username: response.data.username,
-  //       email: response.data.email,
-  //       summoner_name: response.data.summoner_name,
-  //       manner_point: response.data.manner_point,
-  //       reports_for_user: response.data.reports_for_user,
-  //       reports_by_user: response.data.reports_by_user,
-  //       getMyPageDataBoolean: true,
-  //     });
-  //   }
-  // };
-
-  deleteReportData = async () => {
-    // axios.defaults.xsrfCookieName = "csrftoken";
-    // axios.defaults.xsrfHeaderName = "X-CSRFToken";
-
-    // axios.get("/api/token/").then();
-
-    // const response_signin = await axios.post("/api/signin/", {
-    //   username: "test1",
-    //   password: "password",
-    // });
-
-    // if (response_signin.status === 200) {
-    //   axios
-    //     .delete("/api/reports/:id/")
-    //     .then(() => this.setState({ status: "Your report is deleted" }));
-    // }
-    alert("리포트가 삭제되었습니다");
-  };
-
-  onClickApologyWrite = () => {
-    this.setState({ ApologyWrite: true });
-  };
-
-  render() {
-    let redirect = null;
-
-    if (this.state.ApologyWrite === true) {
-      redirect = <Redirect to={`/myApologyWrite`} />;
-    }
-
+  
+  render(){
+    console.log(this.props)
     return (
       <div className="ReportedLog">
-        {redirect}
-        <div className="ReportedLog_Box">
-          <div className="ReportedLog_Text">
-            Recent Reported Log reports_for_user.id :
-            reports_for_user.reported_summoner
-            <br />
-            reports_for_user.tag, reports_for_user.evaluation,
-            reports_for_user.comment
-          </div>
-          <img
-            className="ReportedLog_Pencil"
-            src={Pencil}
-            alt={Pencil}
-            onClick={() => this.onClickApologyWrite()}
-          />
-        </div>
-      </div>
-    );
+         <div className="ReportedLog_Box">
+           <div className="ReportedLog_Text">
+             Recent Reported Log :
+             {this.props.userID}
+             <br />
+             {this.props.tags},
+             {this.props.userEvaluation}
+           </div>
+           <img className = 'ReportedLog_Pencil' alt = 'pencil_img' 
+             src = {process.env.PUBLIC_URL + `/images/icons/icon-pencil.png`}
+             onClick={() => this.props.clicked} />
+         </div>
+       </div>
+   )
   }
 }
 
