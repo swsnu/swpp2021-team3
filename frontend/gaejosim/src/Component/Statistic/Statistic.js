bb051f9e0c0615f586f3834087e1467155bd0dff
ericagong <48196721+ericagong@users.noreply.github.com>
2021-12-03 13:47:20 +0900

    change secrests.tar.enc

--- frontend/gaejosim/src/Component/Statistic/Statistic.js
+++ frontend/gaejosim/src/Component/Statistic/Statistic.js
@@ -7,18 +7,42 @@ import "./Statistic.css";
 
 class Statistic extends Component {
   state = {
-    isLoading: true,
+    getResult : false,
     numReports: 100,
     numPrevents: 5,
   };
 
+  constructor(props) {
+    super(props);
+    this.getStatistics()
+  }
+
+  getStatistics = async () => {
+
+    axios.defaults.xsrfCookieName = "csrftoken";
+    axios.defaults.xsrfHeaderName = "X-CSRFToken";
+
+    axios.get("/api/token/").then();
+
+    const url = "http://localhost:3000/api/statistics/";
+    console.log("call axios.get request");
+    const response = await axios.get('/api/statistics/', {
+    })
+    .then(res => {
+      console.log("response.data")
+      console.log(res.data)
+      this.setState({numReports: res.data.accumulated_reports, numPrevents: res.data.today_reports, getResult: true})
+    })
+  };
+
+
   render() {
     return (
       <div className="Statistic">
-        <div className="AllReports">All Reports</div>
-        <div className="NumReports">{this.state.numReports}</div>
-        <div className="TodayReports">Today Reports</div>
-        <div className="NumPrevents">{this.state.numPrevents}</div>
+        {this.state.getResult && <div className="AllReports">All Reports</div>}
+        {this.state.getResult && <div className="NumReports">{this.state.numReports}</div>}
+        {this.state.getResult && <div className="TodayReports">Today Reports</div> }
+        {this.state.getResult && <div className="NumPrevents">{this.state.numPrevents}</div> }
       </div>
     );
   }
