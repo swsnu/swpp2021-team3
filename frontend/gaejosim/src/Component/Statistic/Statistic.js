bd7bae1fb460d2dfa1483ccd0d932c0b799da6bb
ericagong <48196721+ericagong@users.noreply.github.com>
2021-12-03 13:58:56 +0900

    add statistics alarm compponent to statistics component

--- frontend/gaejosim/src/Component/Statistic/Statistic.js
+++ frontend/gaejosim/src/Component/Statistic/Statistic.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import axios from "axios";
 import "./Statistic.css";
 
-// TODO: /api/statistics/ 만들어 져야 완성 가능
+import { connect } from 'react-redux';
+
+// TODO: css numToAnswer display
 
 class Statistic extends Component {
   state = {
     getResult : false,
     numReports: 100,
     numPrevents: 5,
+    numToAnswer: 0,
   };
 
   constructor(props) {
@@ -29,7 +32,7 @@ class Statistic extends Component {
     .then(res => {
       console.log("response.data")
       console.log(res.data)
-      this.setState({numReports: res.data.accumulated_reports, numPrevents: res.data.today_reports, getResult: true})
+      this.setState({numReports: res.data.accumulated_reports, numPrevents: res.data.today_reports, numToAswer: res.data.not_answered_reports, getResult: true})
     })
   };
 
@@ -40,9 +43,17 @@ class Statistic extends Component {
         {this.state.getResult && <div className="NumReports">{this.state.numReports}</div>}
         {this.state.getResult && <div className="TodayReports">Today Reports</div> }
         {this.state.getResult && <div className="NumPrevents">{this.state.numPrevents}</div> }
+        {this.props.storedisLogin && (this.state.numToAnswer!== 0) && <div>Alert:{this.state.numToAswer}</div>}
       </div>
     );
   }
 }
 
-export default Statistic;
+
+const mapStateToProps = state => {
+  return {
+      storedisLogin : state.userR.login,
+  }
+}
+
+export default connect(mapStateToProps, null)(withRouter(Statistic))
