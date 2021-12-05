db3fddcdf7a4506b70455c61724b8906729a857c
Xena123kiki <ckdnwls1215@snu.ac.kr>
2021-12-05 11:28:40 +0900

    fix: multisearch css styling

--- frontend/gaejosim/src/Component/CommonSearch/CommonSearch.js
+++ frontend/gaejosim/src/Component/CommonSearch/CommonSearch.js
@@ -8,69 +8,82 @@ import tagdiagram_3 from "./tagDiagrams/diagram_3.png";
 import tagdiagram_4 from "./tagDiagrams/diagram_4.png";
 import tagdiagram_5 from "./tagDiagrams/diagram_5.png";
 
-import './CommonSearch.css';
+import "./CommonSearch.css";
 
 // TODO: tag diagram 형성하기
 
 class CommonSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      summonerName: props.summonerName,
+      tier: props.tier,
+      rank: props.rank,
+      mannerPoint: props.mannerPoint,
+      tagValues: props.tagValues,
+      recentResults: props.recentResults,
+      winLose: props.winLose,
+      num: props.num,
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            summonerName : props.summonerName,
-            tier : props.tier,
-            rank : props.rank,
-            mannerPoint : props.mannerPoint,
-            tagValues : props.tagValues,
-            recentResults : props.recentResults,
-            winLose : props.winLose,
-            num: props.num,
-        }
-    }
-    
+  render() {
+    const winLoseArr = this.state.winLose;
+    let idx = -1;
+    const resultViews = this.state.recentResults.map((result) => {
+      idx = idx + 1;
+      return <Result key={idx} result={result} winLose={winLoseArr[idx]} />;
+    });
+    console.log(this.state.num);
 
-    render() {
-        const winLoseArr = this.state.winLose
-        let idx = -1;
-        const resultViews = this.state.recentResults.map((result) => {
-            idx = idx + 1;
-            return (
-                <Result key={idx} result={result} winLose={winLoseArr[idx]} />
-            )
-        })
-        console.log(this.state.num)
-
-        return (
-            <div className='boxStyle1'> 
-                {/* tagdiagrm tagview로 바꾸기 */}
-                {/* <div className = 'tag_values'>
+    return (
+      <div className="boxStyle1">
+        {/* tagdiagrm tagview로 바꾸기 */}
+        {/* <div className = 'tag_values'>
                     <TagView tag_values={this.state.tag_values}/>
                 </div> */}
-                {(this.state.num === 1) && <img className='diagram_1' src={tagdiagram_1} alt="tagdiagram_1" />}
-                {(this.state.num === 2) && <img className='diagram_2' src={tagdiagram_2} alt="tagdiagram_2" />}
-                {(this.state.num === 3) && <img className='diagram_3' src={tagdiagram_3} alt="tagdiagram_3" />}
-                {(this.state.num === 4) && <img className='diagram_4' src={tagdiagram_4} alt="tagdiagram_4" />}
-                {(this.state.num === 5) && <img className='diagram_5' src={tagdiagram_5} alt="tagdiagram_5" />}
+        {this.state.num === 1 && (
+          <img className="diagram_1" src={tagdiagram_1} alt="tagdiagram_1" />
+        )}
+        {this.state.num === 2 && (
+          <img className="diagram_2" src={tagdiagram_2} alt="tagdiagram_2" />
+        )}
+        {this.state.num === 3 && (
+          <img className="diagram_3" src={tagdiagram_3} alt="tagdiagram_3" />
+        )}
+        {this.state.num === 4 && (
+          <img className="diagram_4" src={tagdiagram_4} alt="tagdiagram_4" />
+        )}
+        {this.state.num === 5 && (
+          <img className="diagram_5" src={tagdiagram_5} alt="tagdiagram_5" />
+        )}
 
-                <div className = 'SummonerInfo'>
-                    <p className = 'Summoner'>
-                        <b className = 'nameStyle1'>{this.state.summonerName}</b>
-                        <img className = 'tierImg' alt = 'tier_img' src = {process.env.PUBLIC_URL + `/images/tiers/tier_${this.state.tier}.png`} />
-                        <b className = 'tier'>{this.state.tier}</b> 
-                        <b className = 'rank'>{this.state.rank}</b>
-                    </p>
-                    <div className = 'manner_point'>
-                    <div className = 'manner_point_text'>MP: {this.state.mannerPoint}</div>
-                    </div> 
-                </div>
-
-                <div className= 'recent_results'>
-                    {resultViews}
-                </div>
-                
+        <div className="SummonerInfo">
+          <p className="Summoner">
+            <b className="nameStyle1">{this.state.summonerName}</b>
+            <img
+              className="tierImg"
+              alt="tier_img"
+              src={
+                process.env.PUBLIC_URL +
+                `/images/tiers/tier_${this.state.tier}.png`
+              }
+            />
+            {/* <b className="tier">{this.state.tier}</b>
+            <b className="rank">{this.state.rank}</b>
+            <div className="manner_point_text">
+              MP: {this.state.mannerPoint}
+            </div> */}
+            <div className="tier">
+              {this.state.tier}, {this.state.rank}, MP:{this.state.mannerPoint}
             </div>
-        )
-    }
-}   
-    
+            {/* <div className="recent_results">{resultViews}</div> */}
+          </p>
+          <div className="recent_results">{resultViews}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default CommonSearch;
---
+++ frontend/gaejosim/src/Component/SearchbarForMulti/SearchbarForMulti.test.js
@@ -0,0 +1,85 @@
+import { fireEvent, render } from "@testing-library/react";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { shallow } from "enzyme";
+import SearchbarForMulti from "./SearchbarForMulti";
+// import SearchPage from "../../Page/SearchPage";
+import MultiSearchResultPage from "../../Page/MultiSearchResultPage";
+
+// import MyPage from '../../Page/MyPage';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import userReducer from "../../Store/Reducers/UserReducer";
+
+const rootReducer = combineReducers({
+  userR: userReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+describe("<SearchbarForMulti />", () => {
+  it("should render without errors", () => {
+    const component = shallow(
+      <Provider store={store}>
+        <SearchbarForMulti />
+      </Provider>
+    );
+    const wrapper = component.find(".SearchbarForMulti");
+    expect(wrapper.length).toBe(0);
+  });
+
+  it("should handle search button", () => {
+    const mockSearchButton = jest.fn();
+    const component = shallow(
+      <SearchbarForMulti clickDone={mockSearchButton} />
+    );
+    const wrapper = component.find(".searchForMulti");
+    // wrapper.simulate("click");
+    // expect(mockSearchButton).toHaveBeenCalledTimes(0);
+    expect(wrapper.exists()).toEqual(false);
+  });
+
+  xit("changes input1", () => {
+    const { getByPlaceholderText } = render(
+      <Router>
+        <Provider store={store}>
+          <SearchbarForMulti />
+        </Provider>
+      </Router>
+    );
+    const input1 = getByPlaceholderText(
+      `소환사1님이 방에 참가했습니다.
+      소환사2님이 방에 참가했습니다.
+      소환사3님이 방에 참가했습니다.
+      소환사4님이 방에 참가했습니다.
+      소환사5님이 방에 참가했습니다.
+      또는
+      소환사1, 소환사2, 소환사3, 소환사4, 소환사5`
+    );
+    fireEvent.change(input1, {
+      target: {
+        value: "TDD 배우기",
+      },
+    });
+  });
+});
+
+describe("router search page", () => {
+  test("should pass", () => {
+    const history = createMemoryHistory({ initialEntries: ["/"] });
+    const { getByText } = render(
+      // <Provider>
+      <Router history={history}>
+        <Provider store={store}>
+          <MultiSearchResultPage />
+        </Provider>
+      </Router>
+      // </Provider>
+    );
+    expect(history.location.pathname).toBe("/");
+    fireEvent.click(getByText("검색"));
+    expect(history.location.pathname).toBe("/");
+  });
+});
