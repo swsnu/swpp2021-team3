48baa9f80a6c5706b683f5b76f825996e5ed5d6c
ericagong <48196721+ericagong@users.noreply.github.com>
2021-12-06 12:41:37 +0900

    add email checker and password visibility

--- frontend/gaejosim/src/Component/Signup/Signup.js
+++ frontend/gaejosim/src/Component/Signup/Signup.js
@@ -21,6 +21,11 @@ class SignUp extends Component {
         agreePolicy : false,
     }
 
+    emailChecker = (email) => {
+        let regex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+        return regex.test(email);
+    }
+
     passwordHandler = (passwordCheck) => {
         if(this.state.password === passwordCheck) this.setState({ passwordConfirm : true})
         else return
@@ -35,7 +40,10 @@ class SignUp extends Component {
             alert('모든 필드의 값들을 다 기입해주셔야 회원가입이 가능합니다.')
             return
         }
-        if(!this.state.passwordConfirm) {
+        if(!this.emailChecker(this.state.email)){
+            alert('이메일 형식이 올바르지 않습니다.')
+        }
+        else if(!this.state.passwordConfirm) {
             alert('기입한 비밀번호와 비밀번호 체크 영역의 값이 동일하지 않습니다.')
             return
         }
@@ -96,12 +104,12 @@ class SignUp extends Component {
                     onChange={(event) => this.setState({ summonerID : event.target.value })} />
                 <input
                     className = 'Signup_inputField4'
-                    type = 'string'
+                    type = 'password'
                     placeholder = '비밀번호'
                     onChange={(event) => this.setState({ password : event.target.value })} />
                 <input
                     className = 'Signup_inputField5'
-                    type = 'string'
+                    type = 'password'
                     placeholder = '비밀번호 확인'
                     onChange={(event) => this.passwordHandler(event.target.value)} />
                 {(!this.state.agreePolicy) && <button className = 'AgreeButton'
