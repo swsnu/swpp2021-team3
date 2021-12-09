fe8a6c1db1ea70809187e72a48d12636722dce91
2018-10036 <junepark1211@snu.ac.kr>
2021-12-09 16:10:12 +0900

    fix:update riot key

--- backend/gaejosim/report/urls.py
+++ backend/gaejosim/report/urls.py
@@ -4,19 +4,19 @@ from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-    path("home/", cache_page(60)(views.reports_statistics), name="home"),
+    path("home/", views.reports_statistics, name="home"),
     path("reports/<int:report_id>/", views.delete_report, name="report"),
     path("reports/<int:report_id>/apology/", views.apology, name="apology"),
     path(
         "reports/auth/",
-        cache_page(300)(views.report_authentication),
+        views.report_authentication,
         name="report_auth",
     ),
     path("reports/", views.post_report, name="report"),
-    path("my/reports/", cache_page(300)(views.my_reports), name="my_reports"),
+    path("my/reports/", views.my_reports, name="my_reports"),
     path(
         "my/received_reports/",
-        cache_page(300)(views.my_received_reports),
+        views.my_received_reports,
         name="my_received_reports",
     ),
 ]
